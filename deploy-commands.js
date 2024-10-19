56988c04d9388d59e0b28532c63e4f82c6b17cd9
Nikonej <Nikolai@guss.dk>
2024-10-20 00:40:35 +0200

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -25,7 +25,7 @@ for (const folder of commandFolders) {
 }
 
 // Construct and prepare an instance of the REST module
-const rest = new REST().setToken(token);
+const rest = new REST().setToken(process.env.token);
 
 // and deploy your commands!
 (async () => {
@@ -34,7 +34,7 @@ const rest = new REST().setToken(token);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
-            Routes.applicationCommands(clientId),
+            Routes.applicationCommands(process.env.clientId),
             { body: commands },
         );
 
